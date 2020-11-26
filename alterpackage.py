550c2bb74aa74612c9c34da73beb4785c334cd46
Richie Lee <richie@bzzzt.io>
2020-11-26 22:50:51 +0000

    I will tell you who I am

--- alterpackage.py
+++
@@ -1,8 +0,0 @@
-import sys
-print(sys.argv)
-fin = open("packagetokenised.json", "rt")
-fout = open("package.json", "wt")
-for line in fin:
-  fout.write(line.replace('<<version_number>>', sys.argv[1]))
-fin.close()
-fout.close()
